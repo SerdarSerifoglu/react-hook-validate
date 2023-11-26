feb407b6762dbef47bdf90da1133bcb3d2e4f74c
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-11-26 14:06:23 +0300

    format

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,38 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const App = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onSubmit = (data) => {
     console.log(data);
   };
 
+  const validationRules = [
+    ["name", "age", "surname"],
+    ["name", "surname"],
+    ["name", "age"],
+    ["name", "birthDate"],
+  ];
 
-const validationRules = [["name","age", "surname"],["name", "surname"], ["name","age"],["name","birthDate"]]
-
-  const validationFunction = (data,formData) => {
+  const validationFunction = (data, formData) => {
     for (let i = 0; i < validationRules.length; i++) {
       const element = validationRules[i];
       for (let y = 0; y < element.length; y++) {
         const element2 = element[y];
-       if( formData[element2] === undefined ||  formData[element2] === null || formData[element2] === ""){
-        return console.log(element.join("-"));
-       }
+        if (
+          formData[element2] === undefined ||
+          formData[element2] === null ||
+          formData[element2] === ""
+        ) {
+          return console.log(element.join("-"));
+        }
       }
-      
     }
     return true;
-  }
-
- 
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -33,7 +42,8 @@ const validationRules = [["name","age", "surname"],["name", "surname"], ["name",
         id="name"
         {...register("name", {
           required: "Name is required",
-          validate: (value, formValues) => validationFunction(value,formValues)
+          validate: (value, formValues) =>
+            validationFunction(value, formValues),
         })}
       />
       {errors.name && <p>{errors.name.message}</p>}
@@ -43,7 +53,8 @@ const validationRules = [["name","age", "surname"],["name", "surname"], ["name",
         id="surname"
         {...register("surname", {
           required: "Surname is required",
-          validate: (value, formValues) => validationFunction(value,formValues)
+          validate: (value, formValues) =>
+            validationFunction(value, formValues),
         })}
       />
       {errors.surname && <p>{errors.surname.message}</p>}
@@ -54,7 +65,8 @@ const validationRules = [["name","age", "surname"],["name", "surname"], ["name",
         type="number"
         {...register("age", {
           required: "Age is required",
-          validate: (value, formValues) => validationFunction(value,formValues)
+          validate: (value, formValues) =>
+            validationFunction(value, formValues),
         })}
       />
       {errors.age && <p>{errors.age.message}</p>}
@@ -65,7 +77,8 @@ const validationRules = [["name","age", "surname"],["name", "surname"], ["name",
         type="date"
         {...register("birthDate", {
           required: "Birth date is required",
-          validate: (value, formValues) => validationFunction(value,formValues)
+          validate: (value, formValues) =>
+            validationFunction(value, formValues),
         })}
       />
       {errors.birthDate && <p>{errors.birthDate.message}</p>}
